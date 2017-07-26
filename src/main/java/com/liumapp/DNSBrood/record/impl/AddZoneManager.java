4c2be71a5551d9dc91e5abdb1cbfa298ad5bc0d9
liumapp <liumapp.com@gmail.com>
2017-07-26 15:21:00 +0800

    doubel key map 不使用userNumber作为key

--- src/main/java/com/liumapp/DNSBrood/record/impl/AddZoneManager.java
+++ src/main/java/com/liumapp/DNSBrood/record/impl/AddZoneManager.java
@@ -47,7 +47,7 @@ public class AddZoneManager implements Manager {
                 return "PARSE ERROR";
             }
 
-            Zones zone = new Zones(zonesPattern.getUserNumber(),zonesPattern.getTexts().toString() , zonesPattern.getTargetIp() , "A" , new Date().getTime() , new Date().getTime());
+            Zones zone = new Zones(zonesPattern.getUserIp(),zonesPattern.getTexts().toString() , zonesPattern.getTargetIp() , "A" , new Date().getTime() , new Date().getTime());
 
             /**
              * save to db
@@ -60,11 +60,11 @@ public class AddZoneManager implements Manager {
              * put to dnsjava
              */
             for (Pattern pattern : zonesPattern.getPatterns()) {
-                customAnswerPatternProvider.getDomainPatterns().put(zonesPattern.getUserNumber(), pattern, zonesPattern.getTargetIp());
+                customAnswerPatternProvider.getDomainPatterns().put(zonesPattern.getUserIp(), pattern, zonesPattern.getTargetIp());
             }
 
             for (String text : zonesPattern.getTexts()) {
-                customAnswerPatternProvider.getDomainTexts().put(zonesPattern.getUserNumber(), text, zonesPattern.getTargetIp());
+                customAnswerPatternProvider.getDomainTexts().put(zonesPattern.getUserIp(), text, zonesPattern.getTargetIp());
             }
 
             return "SUCCESS, " + zonesPattern.getPatterns().size() + " patterns added.";
